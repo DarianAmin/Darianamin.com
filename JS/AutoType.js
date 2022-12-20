35631a21a5f47ffc46838508a90f6ca41b8b26d1
Darian Amin <70491110+DarianAmin@users.noreply.github.com>
2022-12-20 18:09:09 +0000

    Add files via upload

--- JS/AutoType.js
+++ JS/AutoType.js
@@ -1,5 +1,5 @@
 var typed=new Typed(".auto-type",{
-    strings:["Web Developer","Web Designer","Freelancer"],
+    strings:["Darian Amin", "a Photographer"],
     typeSpeed:150,
     backSpeed:150,
 })
